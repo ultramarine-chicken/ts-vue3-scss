89d7f9ed5d8e406c2b3ba162c6bc3cedae529b7f
ultramarine-chicken <ultramarine.chicken@gmail.com>
2021-12-05 13:09:17 +0900

    環境を整えた”

---
+++ dist/js/script.js
@@ -0,0 +1,7 @@
+"use strict";
+let num = 3;
+console.log('Hello, ' + num + '!!');
+function hello(name) {
+    console.log('Hello, ' + name + '!');
+}
+hello('MyName');
---
+++ src/ts/script.ts
@@ -0,0 +1,7 @@
+let num: number = 3;
+console.log('Hello, ' + num + '!!');
+
+function hello(name: string): void{
+    console.log('Hello, ' + name + '!');
+}
+hello('MyName');
