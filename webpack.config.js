094a74bbb30ada517840157148bd3f3c8b9e9d75
ultramarine-chicken <ultramarine.chicken@gmail.com>
2021-12-11 21:53:59 +0900

    bundleをdist直下に

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     entry: './src/js/index.ts',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist/js'),
+        path: path.resolve(__dirname, 'dist'),
         publicPath: ''
     },
     module: {
